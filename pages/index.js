ee49eb0931f9aacc279919c3d0e71b18aa965d92
Prabhjot Singh <pjsingh1359@gmail.com>
2022-10-03 14:11:05 -0700

    add space

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Fast Feedback
+          Fast Feedback 
         </h1>
 
         <p className={styles.description}>
